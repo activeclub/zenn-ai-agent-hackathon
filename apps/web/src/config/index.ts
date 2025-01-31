b2c56d01c818f30d89f14ece8fca3a8d62f59a1f
Naoto Nishizaka <nszk.nao@gmail.com>
2025-02-01 02:37:20 +0900

    Update production API base URL

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://api.wondy.io"
+    ? "https://web-197674220017.asia-northeast1.run.app"
     : "http://localhost:3000";
